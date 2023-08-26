498e057d46f8bd2755e4baafc601ade240ce5a95
Narek <narekbayanduryan16@gmail.com>
2023-08-26 10:37:46 +0300

    Adding working business logic.

--- calc.php
+++ calc.php
@@ -1,5 +1,31 @@
 <?php 
-if (empty($_GET)) {
-	return 'Ничего не передано!';
-}
+	if (empty($_GET)) {
+		return 'Ничего не передано!';
+	}
+
+	if (empty($_GET['operation'])) {
+		return 'Не передана операция!';
+	}
+
+	if (empty($_GET['x1']) || empty($_GET['x2'])) {
+		return 'Не переданы аргументы!';
+	}
+
+	$x1 = $_GET['x1'];
+	$x2 = $_GET['x2'];
+
+	$expression = $x1 . ' ' . $_GET['operation'] . ' ' . $x2 . ' = ';
+
+	switch ($_GET['operation']) {
+		case '+':
+			$result = $x1 + $x2;
+			break;
+		case '-':
+			$result = $x1 - $x2;
+			break;
+		default:
+			return 'Операция не поддерживается.';
+	}
+
+	return $expression . $result;
 ?>
--- index.php
+++ index.php
@@ -6,8 +6,8 @@
 	<form action="/result.php">
 		<input type="text" name="x1">
 		<select name='operation'>
-			<option value="+"></option>
-			<option value="-"></option>
+			<option value="+">+</option>
+			<option value="-">-</option>
 		</select>
 		<input type="text" name="x2">
 		<input type="submit" name="Посчитать">
